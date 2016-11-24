0de207f744cb18ab8021eb7bfe5e4d12f4fc0185
pchan52 <pchan52611625@gmail.com>
2016-11-24 12:39:59 +0900

    stage outline create

---
+++ Assets/prototypeVR/Scripts/Door.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.transform.parent.GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		anim.SetBool("isOpen",true);
+	}
+
+	void OnTriggerExit(Collider other){
+		anim.SetBool("isOpen",false);
+	}
+}
