f0fb230dacc90864482e86d172f941d07ab08558
pchan52 <pchan52611625@gmail.com>
2016-11-30 01:44:34 +0900

    コード修正

--- Assets/prototypeVR/Scripts/Door.cs
+++ Assets/prototypeVR/Scripts/Door.cs
@@ -5,21 +5,18 @@ public class Door : MonoBehaviour {
 
 	Animator anim;
 
-	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		anim = gameObject.transform.parent.GetComponent<Animator> ();
 	}
 
-	// Update is called once per frame
-	void Update () {
-
-	}
-
-	void OnTriggerEnter(Collider other){
+	void OnTriggerEnter(Collider other)
+	{
 		anim.SetBool("isOpen",true);
 	}
 
-	void OnTriggerExit(Collider other){
+	void OnTriggerExit(Collider other)
+	{
 		anim.SetBool("isOpen",false);
 	}
 }
---
+++ Assets/prototypeVR/Scripts/Weapon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour 
+{
+	[SerializeField] AudioClip audioclip;
+	[SerializeField] GameObject bullet;
+	AudioSource audioSource;
+
+	void Start () 
+	{
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.clip = audioclip;
+	}
+	
+	void Update () 
+	{
+		if ( Input.GetButtonDown("Fire1") ) 
+		{
+			Shot ();
+			//bullet移動のメソッドを呼ぶ
+		} 
+	}
+
+	void Shot()
+	{
+		audioSource.Play();
+		var newBullet = Instantiate(bullet);
+		newBullet.transform.position = transform.position;
+		newBullet.transform.rotation = transform.rotation;
+	}
+}
